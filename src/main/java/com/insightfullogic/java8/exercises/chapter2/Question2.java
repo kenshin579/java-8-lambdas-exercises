039bee6449fada5bfc36ffd79f1b0c1c9185e24c
kenshin579 <kenshin579@hotmail.com>
2015-12-28 21:54:38 +0900

    reformat

--- src/main/java/com/insightfullogic/java8/exercises/chapter2/Question2.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter2/Question2.java
@@ -2,11 +2,7 @@ package com.insightfullogic.java8.exercises.chapter2;
 
 import com.insightfullogic.java8.exercises.Exercises;
 
-import javax.swing.text.DateFormatter;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
-
-import static java.lang.ThreadLocal.withInitial;
 
 public class Question2 {
 
