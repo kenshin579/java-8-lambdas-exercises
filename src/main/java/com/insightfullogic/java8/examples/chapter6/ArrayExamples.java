8fb2506ddda7e752e100eee53b1887f6a3da50c9
kenshin579 <kenshin579@hotmail.com>
2016-01-23 14:12:11 +0900

    스터디완료(chap6)

--- src/main/java/com/insightfullogic/java8/examples/chapter6/ArrayExamples.java
+++ src/main/java/com/insightfullogic/java8/examples/chapter6/ArrayExamples.java
@@ -6,16 +6,30 @@ import java.util.stream.IntStream;
 public class ArrayExamples {
 
     // BEGIN simpleMovingAverage
+
+    /**
+     * @param values
+     * @param n      구간을 의미함
+     * @return
+     */
     public static double[] simpleMovingAverage(double[] values, int n) {
         double[] sums = Arrays.copyOf(values, values.length); // <1>
-        Arrays.parallelPrefix(sums, Double::sum); // <2> //todo: 이해 안됨
+        Arrays.parallelPrefix(sums, Double::sum); // <2>
+
+        //sums: 0.0, 1.0, 3.0, 6.0, 10.0, 13.5
+
         int start = n - 1;
+        System.out.println("start: " + start + " sums.length: " + sums.length);
         return IntStream.range(start, sums.length) // <3>
                 .mapToDouble(i -> {
                     double prefix = i == start ? 0 : sums[i - n];
+                    System.out.println("i: " + i + " == start: " + start + " ? ==> prefix: " + prefix);
+                    System.out.println("(sums[" + i + "]: " + sums[i] + " - prefix:" + prefix + ") / " + n + " ==> result: " + (sums[i] - prefix) / n);
                     return (sums[i] - prefix) / n; // <4>
                 })
                 .toArray(); // <5>
+
+        //result: 1, 2, 3, 3,5
     }
     // END simpleMovingAverage
 
@@ -50,7 +64,16 @@ public class ArrayExamples {
     }
 
     public static void main(String[] args) {
-        arrayParallelPrefix();
+//        arrayParallelPrefix();
+
+//        for (double value : parallelInitialize(5)) {
+//            System.out.println(value);
+//        }
+
+        double[] array = {0, 1, 2, 3, 4, 3.5};
+        for (double value : simpleMovingAverage(array, 3)) {
+            System.out.println(value);
+        }
 
     }
 }
